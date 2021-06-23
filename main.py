b58264d8ff3d0ecbb5a2ade941759b4ac50e0d9c
Ramon Rodrigo <ramonrodrigoss@gmail.com>
2021-06-23 00:11:58 +0000

    Fiz o tratamento da base de dados

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ tabela = pd.read_csv("telecom_users.csv")
 # PASSO 3
 #Colunas Unnamed é inutil - > vamos deletar
 tabela = tabela.drop("Unnamed: 0", axis=1)
-display(tabela)
 #Ver se tem coluna que era pra ser um numero e reconhecida com um texo
 
 tabela["TotalGasto"] = pd.to_numeric(tabela["TotalGasto"], errors="coerce")# transforma em numerico, depois se der erro ele vai deixar em branco
+#Colunas vazio (NaN)
+#1º colunas completamente vazias
+tabela = tabela.dropna(how="all", axis=1)
+#2º linhas com alguma celula vazias
+tabela = tabela.dropna(how="any", axis=0)
 print(tabela.info())
 
-#Colunas vazio (NaN)
-#colunas completamente vazias
-#linhas vazias
+
 
